755e49b1005f3eaa617d991d9554a115632d6d8c
Dima Dyomin <mambus.skruj@gmail.com>
2016-08-06 17:51:07 +0300

    create superuser, add models to dj-admin

--- simply_test/events/admin.py
+++ simply_test/events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Area, City, Event
+
+admin.site.register(Area)
+admin.site.register(City)
+admin.site.register(Event)
+

1b44682f7642384cfaf3a2094631640910a4ef28
Dima Dyomin <mambus.skruj@gmail.com>
2016-08-06 18:25:14 +0300

    add areas and cities, create events in dj-admin

--- simply_test/events/models.py
+++ simply_test/events/models.py
@@ -5,13 +5,22 @@ from django.db import models
 class Area(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class City(models.Model):
     name = models.CharField(max_length=100)
     city_area = models.ForeignKey(Area)
+
+    def __str__(self):
+        return self.name
 
 class Event(models.Model):
     name = models.CharField(max_length=300)
     date = models.DateTimeField()
     isFree = models.BooleanField(default=False)
     city_event = models.ForeignKey(City)
+
+    def __str__(self):
+        return self.name
 

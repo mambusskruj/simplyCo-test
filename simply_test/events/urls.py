177d2f7dad17535a454da7f4635eda2571b6ece2
Dima Dyomin <mambus.skruj@gmail.com>
2016-08-08 16:23:56 +0300

    filter by paid type

--- simply_test/events/urls.py
+++ simply_test/events/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^type$', views.filter_paid, name='paid'),
 ]
--- simply_test/events/views.py
+++ simply_test/events/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Area, City, Event
 from datetime import datetime
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,19 @@ def index(request):
         'events' : event_list,
     }
     return render(request, 'events/events.html', context)
+
+def filter_paid(request):
+    if request.POST:
+        if request.POST.get('type')=='free':
+            free_events = Event.objects.filter(isFree=True).filter(date__gte=datetime.now()).order_by('date') 
+            context = {
+                'events' : free_events,
+            }
+            return render(request, 'events/events.html', context)
+        elif request.POST.get('type')=='paid':
+            paid_events = Event.objects.filter(isFree=False).filter(date__gte=datetime.now()).order_by('date')
+            context = {
+                'events' : paid_events,
+            }
+            return render(request, 'events/events.html', context)
+        return HttpResponseRedirect('/events')

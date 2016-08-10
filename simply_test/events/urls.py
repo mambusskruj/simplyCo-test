e832cc9ac1217f9a8cdf9d53d693b7cdaea00a68
Dima Dyomin <mambus.skruj@gmail.com>
2016-08-10 03:16:16 +0300

    beautiful url filters, select2

--- simply_test/events/urls.py
+++ simply_test/events/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^type$', views.filter_paid, name='paid'),
+    url(r'^type/$', views.filter_paid, name='paid'),
+    url(r'^(?P<filter1>\w+)/(?P<value1>\w+)/$', views.filter, name='filter'),
+    url(r'^(?P<filter1>\w+)/(?P<value1>\w+)/(?P<filter2>\w+)/(?P<value2>\w+)/$', views.filter2, name='filter2'),
 ]
+
+#(?P<type>\w+)
--- simply_test/events/views.py
+++ simply_test/events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, redirect
 
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Area, City, Event
@@ -13,18 +13,97 @@ def index(request):
     }
     return render(request, 'events/events.html', context)
 
+# def filter_paid(request, type='free'):
+#     if request.POST:
+#         if request.POST.get('type')=='free':
+#             free_events = Event.objects.filter(isFree=True).filter(date__gte=datetime.now()).order_by('date') 
+#             context = {
+#                 'events' : free_events,
+#                 'type' : type,
+#             }
+#             return render(request, 'events/events.html', context)
+#         elif request.POST.get('type')=='paid':
+#             type='paid'
+#             paid_events = Event.objects.filter(isFree=False).filter(date__gte=datetime.now()).order_by('date')
+#             context = {
+#                 'events' : paid_events,
+#                 'type' : type,
+#             }
+#             return render(request, 'events/events.html', context)
+
+#     return HttpResponseRedirect('/events')
+
+
 def filter_paid(request):
     if request.POST:
         if request.POST.get('type')=='free':
+            if request.POST.get('city')!='all':
+                return redirect('/events/type/free/city/'+request.POST.get('city'))
+            return redirect('/events/type/free')
+            
+        elif request.POST.get('type')=='paid':
+            if request.POST.get('city')!='all':
+                return redirect('/events/type/paid/city/'+request.POST.get('city'))
+            return redirect('/events/type/paid')
+
+        if request.POST.get('city')!='all':
+            return redirect('/events/city/'+request.POST.get('city'))
+
+        return HttpResponseRedirect('/events')
+
+
+def filter(request, filter1, value1):
+    if filter1=='type':
+        if value1=='free':
             free_events = Event.objects.filter(isFree=True).filter(date__gte=datetime.now()).order_by('date') 
             context = {
                 'events' : free_events,
             }
             return render(request, 'events/events.html', context)
-        elif request.POST.get('type')=='paid':
-            paid_events = Event.objects.filter(isFree=False).filter(date__gte=datetime.now()).order_by('date')
+
+        if value1=='paid':
+            free_events = Event.objects.filter(isFree=False).filter(date__gte=datetime.now()).order_by('date') 
             context = {
-                'events' : paid_events,
+                'events' : free_events,
             }
             return render(request, 'events/events.html', context)
-        return HttpResponseRedirect('/events')
+            
+    if filter1=='city':
+        city_events = Event.objects.filter(city_event=City.objects.get(slug=value1))
+        context = {
+                'events' : city_events,
+            }
+        return render(request, 'events/events.html', context)
+    return HttpResponseRedirect('/events')
+
+
+def filter2(request, filter1, filter2, value1, value2):
+    events = []
+    if filter1=='type':
+        if value1=='free':
+            events = Event.objects.filter(isFree=True).filter(date__gte=datetime.now()).order_by('date')
+        if value1=='paid':
+            events = Event.objects.filter(isFree=False).filter(date__gte=datetime.now()).order_by('date')
+        if filter2=='city':
+            city_events = events.filter(city_event=City.objects.get(slug=value2))
+            context = {
+                'events' : city_events,
+            }
+            return render(request, 'events/events.html', context)
+
+    if filter1=='city':
+        city_events = Event.objects.filter(city_event=City.objects.get(slug=value1))
+        if filter2=='type':
+            if value2=='free':
+                events = city_events.filter(isFree=True).filter(date__gte=datetime.now()).order_by('date')
+                context = {
+                    'events' : events,
+                }
+                return render(request, 'events/events.html', context)
+            if value2=='paid':
+                events = city_events.filter(isFree=False).filter(date__gte=datetime.now()).order_by('date')
+                context = {
+                    'events' : events,
+                }
+                return render(request, 'events/events.html', context)
+    return HttpResponseRedirect('/events')
